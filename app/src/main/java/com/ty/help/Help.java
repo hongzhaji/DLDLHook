a349638089d74c81a191c9cfcb0e407349f00f1d
hongzhaji <pltyhzj@163.com>
2020-07-13 18:53:03 +0800

    增加 大富翁

--- app/src/main/java/com/ty/help/Help.java
+++ app/src/main/java/com/ty/help/Help.java
@@ -4,6 +4,8 @@ import android.os.Debug;
 import android.os.Environment;
 import android.util.Log;
 
+import com.ty.util.DownloadUtil;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -22,21 +24,17 @@ public class Help {
     }
         public  void  getResoure()
         {
-            
+            Log.i("tyty"," --- getResoure");
         }
 
         public  void  putResonre()
         {
-            try {
-                File file = new File(rootPath+this.name);
-                FileWriter fileWriter = new FileWriter(file);
+            //rootPath+"Android/data/com.qidian.dldl.huawei/scripts/app/ui/dialogs/" ---- "QUIDialogMonopolyFingerguessing.lua" "QUIDialogMonopoly.lua" 大富翁
+            downFile("http://47.107.87.28:8080/JsonData/QUIDialogMonopolyFingerguessing.lua",rootPath+"Android/","QUIDialogMonopolyFingerguessing.lua");
+            downFile("http://47.107.87.28:8080/JsonData/QQUIDialogMonopoly.lua",rootPath+"Android/","QUIDialogMonopoly.lua");
 
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            //加速  ootPath+"Android/data/com.qidian.dldl.huawei/scripts/app/"   MyApp.lua
+            //downFile("http://47.107.87.28:8080/JsonData/MyApp.lua",rootPath+"Android/data/com.qidian.dldl.huawei/scripts/app/","MyApp.lua");
         }
 
         public void  helphelp()
@@ -57,6 +55,28 @@ public class Help {
 
             File file = new File(rootPath+"Android/data/com.qidian.dldl.huawei/scripts/app/MyApp.lua");
             return  file.exists();
+        }
+
+        private  void  downFile(String url,String fileDir,String fileName)
+        {
+            DownloadUtil.get().download(url,fileDir,fileName,new DownloadUtil.OnDownloadListener()
+            {
+
+                @Override
+                public void onDownloadSuccess(File file) {
+
+                }
+
+                @Override
+                public void onDownloading(int progress) {
+
+                }
+
+                @Override
+                public void onDownloadFailed(Exception e) {
+
+                }
+            });
         }
 
 }
